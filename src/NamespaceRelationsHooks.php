5f9d242871216b6ea275a2abdd8411192d255b5e
Umherirrender <umherirrender_de.wp@web.de>
2026-02-22 13:12:35 +0100

    Change navigation from 'namespaces' to 'associated-pages'
    Deprecated in core
    
    Follow-Up: Ifdd3f51c71175913170daf866be11eeefada3e11
    Change-Id: Ice4980adad0269740d9c21fbaf7f4896485e8abf

--- src/NamespaceRelationsHooks.php
+++ src/NamespaceRelationsHooks.php
@@ -8,7 +8,11 @@ class NamespaceRelationsHooks {
 	 * @param array &$navigation
 	 */
 	public static function onSkinTemplateNavigationUniversal( $skinTemplate, &$navigation ) {
+		$nsKey = $skinTemplate->supportsMenu( 'namespaces' ) ? 'namespaces' : 'associated-pages';
+		if ( !isset( $navigation[$nsKey] ) ) {
+			$navigation[$nsKey] = [];
+		}
 		$nsRelations = new NamespaceRelations();
-		$nsRelations->injectTabs( $skinTemplate, $navigation['namespaces'] );
+		$nsRelations->injectTabs( $skinTemplate, $navigation[$nsKey] );
 	}
 }
